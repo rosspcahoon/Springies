5f72d0796997c9c88e8c4a3adf649c27d0f6238a
Ross Cahoon <ross.p.cahoon@gmail.com>
2013-02-06 11:02:46 -0500

    Fixed issues in Factory with add(Force)

---
+++ src/simulation/WallRepulsionForce.java
@@ -0,0 +1,48 @@
+package simulation;
+
+import util.Vector;
+
+public class WallRepulsionForce extends Force {
+	Vector myRepulsion;
+	double myExponent;
+
+	public WallRepulsionForce(int wallID, double magnitude, double exponent)
+	{
+		double angle=((wallID+1)*90)%360;
+		myRepulsion=new Vector(angle,magnitude);
+		myExponent=exponent;
+	}
+	@Override
+	public void applyForce(Mass m) {
+		Vector scaledForce=new Vector(myRepulsion);
+		double distance=0;
+		switch((int)scaledForce.getDirection()%360)
+		{
+			case 0:
+			{
+				distance=Vector.distanceBetween(m.getCenter().y,Model.SIZE.height);
+				break;
+			}
+			case 90:
+			{
+				distance=m.getCenter().x;
+				break;
+			}
+			case 180:
+			{
+				distance=m.getCenter().y;
+				break;
+			}
+			case 270:
+			{
+				distance=Vector.distanceBetween(m.getCenter().x,Model.SIZE.width);
+				break;
+			}
+		}
+		
+		scaledForce.scale(1/(Math.pow(distance,myExponent)));
+		
+		m.applyForce(scaledForce);
+	}
+
+}
